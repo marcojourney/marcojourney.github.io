46c8463c3f340085073d4e19f30e2d0481034970
Marco <mornsophannamis@gmail.com>
2023-10-11 14:23:26 +0700

    some modification

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { initGA, logPageView } from './ga';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  React.useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
+  
   return (
     <>
       <Head>
